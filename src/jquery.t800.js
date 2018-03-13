5d9a99cd7e3d15744f23b3ea0f669c82e5fce338
Andrés G. Carmona <ing.andres.carmona@gmail.com>
2018-03-13 02:32:42 -0600

    Change name of plugin from parsely to validator. Implemented the initial code of the plugin. Only including three methods for validation: required, email and min. Improved format function.

--- src/jquery.t800.js
+++ src/jquery.t800.js
@@ -95,18 +95,35 @@
     var _VERSION = '0.1.0';
     var _methods = {
         init: function(options) {
-            return new $.parsely(this, options);
+            return new $.validator(this, options);
         },
         validate: function(options) {
-            var parsely = _methods.init.apply(this);
-            this.valid  = parsely.validate(options);
+            var validator = _methods.init.apply(this);
+            this.valid    = validator.validate(options);
 
             return this;
+        },
+        min: function(msg, params) {
+            return validate(
+                $(this),
+                function(obj) {
+                    if($(obj).val().trim()) {
+                        return $(obj).val().trim().length >= params;
+                    }
+
+                    return false;
+                },
+                msg || $(this).data('min') || $.validator.settings.messages.min,
+                {
+                    field: $(this).attr('name'),
+                    min: params
+                }
+            );
         }
     };
 
-    $.parsely = function(element, options) {
-        var parsely  = this,
+    $.validator = function(element, options) {
+        var validator  = this,
             $element = $(element),
             element  = element,
             defaults = {
@@ -126,52 +143,80 @@
                     'input[type="email"]',
                     'input[type="tel"]'
                 ],
+                // Validate form on submission.
+                autoValidate: true,
                 messages:{
                     required: '${field} is required.',
-                    email   : 'Email format not valid.'
+                    email   : 'Email format not valid.',
+                    min     : '${field} requires to be at least ${min} characters long'
                 },
+                rules: {},
                 emailRE: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
             };
         
-        parsely.version     = _VERSION;
-        parsely.initialized = false;
-        parsely.settings    = {};
-        parsely.el          = element;
-        parsely.$el         = $element;
-        parsely.valid       = true;
-        parsely.$inputs     = [];
-        parsely.out         = '';
+        validator.version     = _VERSION;
+        validator.initialized = false;
+        validator.settings    = {};
+        validator.el          = element;
+        validator.$el         = $element;
+        validator.valid       = true;
+        validator.$inputs     = {};
+        validator.inputs      = {};
+        validator.out         = '';
 
-        parsely.init = function() {
-            $.parsely.settings    = parsely.settings = $.extend({}, defaults, options);
-            $.parsely.initialized = true;
-        };
-
-        parsely.init();
-    };
-
-    $.parsely.prototype = {
-        validate: function(options) {
-            var self = this;
+        validator.init = function() {
+            $.validator.settings = validator.settings = $.extend({}, defaults, options);
+            var inputs           = validator.$el.find(validator.settings.elements.join(', '));
 
             // Get all inputs and attach them the rules passed in the element's class attribute.
-            var $inputs = this.$el.find(this.settings.elements.join(', '));
-            $inputs.each(function(index) {
-                var $el = $(this);
-                if($el.data('validation')) {
-                    $inputs[index].rules = self.getRules($el.data('validation'));
-                }
-            });
+            validator.$inputs = {};
+            validator.rules   = $.validator.settings.rules;
 
-            this.$inputs = $inputs;
+            // Export $inputs array.
+            $.validator.inputs = {};
+
+            for(var i = 0, len = inputs.length; i < len; i += 1) {
+                validator.$inputs[inputs[i].id]  = inputs[i];
+                
+                $.validator.inputs[inputs[i].id] = {
+                    name      : inputs[i].id,
+                    hasErrors : false,
+                    rules     : []
+                };
+            }
+
+            // If element is a form, prevent submission before validation.
+            if(element.tagName.toLowerCase() === 'form') {
+                $(element).on('submit', function() {
+                    return false;
+                }).on('submit', function() {
+                    validator.validate();
+                });
+            }
+            
+            $.validator.initialized = true;
+        };
+
+        validator.init();
+    };
+
+    $.validator.prototype = {
+        validate: function(options) {
             this.clearErrors();
-            return this._do();
+            return this._validate();
         },
+
+        /**
+         * Get rules from data-validation attribute.
+         * 
+         * @param  Array validations Array of validations rules.
+         * @return Object validations rules object generated.
+         */
         getRules: function(validations) {
             var rulesObj = {},
                 rules    = [];
             
-            validations = validations.match(/parsely\[(.*)\]/)[1];
+            validations = validations.match(/validator\[(.*)\]/)[1];
             rules       = validations.match(/[^,\s]([^,]*)[^,\s]*/g);
 
             for(var i = 0, rule = null, len = rules.length; i < len; i+= 1) {
@@ -190,10 +235,17 @@
             return rulesObj;
         },
         clearErrors: function() {
-            $('.parsely.error').remove();
+            // Rease hasError flag in inputs.
+            if($.validator.inputs) {
+                for(var i in $.validator.inputs) {
+                    $.validator.inputs[i].hasErrors = false;
+                }
+            }
+
+            $('.validator.error, .error-list').remove();
         },
-        _do: function() {
-            // Assume everything is ok.
+        _validate: function() {
+            // Assume everything is OK.
             var valid = true;
 
             if(!this.$inputs) {
@@ -201,39 +253,71 @@
                 return false;
             }
 
-            for(var i = 0, len = this.$inputs.length; i < len; i += 1) {
-                var input  = this.$inputs[i],
-                    $input = $(input);
+            for(var r in this.rules) {
+                // If there is a rule defined for this field, then validate it.
+                if(this.$inputs[r]) {
+                    var input  = this.$inputs[r],
+                        $input = $(input),
+                        rules  = this.rules[r];
 
-                if(input.rules) {
-                    // First evaluate the required rule if it exists in the rules object.
-                    if(input.rules['required'] && $input.val().trim() === '') {
-                        if(!$input.required()) {
-                            valid = false;
+                    for(var i = 0, len = rules.length; i < len; i += 1) {
+                        var ruleObj   = rules[i];
+                        var rule      = null;
+                        var message   = '';
+                        var validator = null;
+                        var params    = null;
 
-                            if($.parsely.settings.validationMode === 'all') continue;
-                            else break;
+                        if($.isPlainObject(ruleObj)) {
+                            $.each(ruleObj, function(r, v) {
+                                rule      = r;
+
+                                if($.isPlainObject(v)) {
+                                    message   = v.message || '';
+                                    validator = v.validate || null;
+                                }
+                                else {
+                                    params = v;
+                                }
+                            });
                         }
-                    }
-
-                    for(var r in input.rules) {
-                        var rule = input.rules[r];
-
-                        switch(r) {
-                            case 'email':
-                                valid = $input.email();
-                                break;
+                        else {
+                            rule = ruleObj;
                         }
 
-                        if(!valid) {
-                            if($.parsely.settings.validationMode === 'all') continue;
-                            else break;
+                        if(validator && $.type(validator) === 'function') {
+                            valid = validate(
+                                $input,
+                                validator,
+                                message || $input.data('required') || $.validator.settings.messages.required
+                            );
+                        }
+                        else {
+                            switch(rule) {
+                                case 'required':
+                                    valid = $input.required(message);
+                                    break;
+                                case 'email':
+                                    valid = $input.email(message);
+                                    break;
+                                case 'min':
+                                    valid = $input.validate('min', message, params);
+                                    break;
+                            }
+                        }
+
+                        // Just run the first rule.
+                        if(valid && ($.validator.settings.validationMode === 'all' || $.validator.settings.validationMode === 'single_all')) {
+                            continue;
+                        }
+                        else {
+                            break;
                         }
                     }
                 }
 
-                if($.parsely.settings.validationMode === 'all') continue;
-                else if(!valid) return valid;
+                if(!valid && ($.validator.settings.validationMode === 'single' || $.validator.settings.validationMode === 'single_all')) {
+                    break;
+                }
             }
 
             return valid;
@@ -241,7 +325,7 @@
     };
 
     $.fn.extend({
-        parsely: function(method) {
+        validate: function(method) {
             var args = arguments;
 
             return this.each(function() {
@@ -250,7 +334,7 @@
                 }
                 else if(typeof method === 'object' || !method) {
                     // Only applies to form elements.
-                    if($.inArray(this.tagName.toLowerCase(), ['form', 'input'])) {
+                    if($.inArray(this.tagName.toLowerCase(), ['form', 'input']) > -1) {
                         return _methods.init.apply(this, args);
                     }
                 }
@@ -262,7 +346,7 @@
                 function(obj) {
                     return $(obj).val() !== null && !!$(obj).val().length;
                 },
-                msg || $(this).data('required') || $.parsely.settings.messages.required
+                msg || $(this).data('required') || $.validator.settings.messages.required
             );
         },
         email: function(msg) {
@@ -270,46 +354,67 @@
                 this,
                 function(obj) {
                     if($(obj).val()) {
-                        return $(obj).val() !== null && $(obj).val().trim().match($.parsely.settings.emailRE) !== null;
+                        return $(obj).val() !== null && $(obj).val().trim().match($.validator.settings.emailRE) !== null;
                     }
 
                     return $(obj).required();
                 },
-                msg || $(this).data('email') || $.parsely.settings.messages.email
+                msg || $(this).data('email') || $.validator.settings.messages.email
             );
         }
     });
 
-    function validate($obj, validation, message) {
-        if(!$.parsely.initialized) $obj.parsely();
+    function validate($obj, validation, message, replacement) {
+        if(!$.validator.initialized) $obj.validator();
 
         // Clear previous error messages.
-        $('.' + $obj.attr('name') + '.parsely.error').remove();
+        $('.' + $obj.attr('name') + '.validator.error').remove();
 
-        var $inputs = $obj.filter($.parsely.settings.elements.join(','));
-
-        for(var i = 0, len = $inputs.length; i < len; i += 1) {
-            // If validation is not passed throw error message.
-            if(!validation($inputs[i])) {
-                throwError($inputs[i], message);
-                return false;
-            }
-        };
+        if(!validation($obj)) {
+            throwError($obj, message, replacement);
+            return false;
+        }
 
         return true;
     }
 
-    function throwError(obj, message) {
-        if($.parsely.settings.outputMode === 'inline') {
-            var $error = $('<span class="' + $(obj).attr('name') + ' parsely error">' + format(obj, message) + '</span>');
-            $(obj).after($error.hide());
+    function throwError(obj, message, replacement) {
+        if($.validator.settings.outputMode === 'inline') {
+            var $error = $('<li><span class="' + $(obj).attr('name') + ' validator error">' + format(obj, message, replacement) + '</span></li>');
+            var $ul    = null;
 
-            $error.show().fadeOut(0).fadeIn('fast');
+            if($.validator.inputs[$(obj).attr('name')].hasErrors === false) {
+                $ul = $('<ul class="error-list" data-id="' + $(obj).attr('name') + '-errors"></ul>');
+                $(obj).after($ul);
+            }
+            else {
+                $ul = $('ul.error-list[data-id="' + $(obj).attr('name') + '-errors"]');
+            }
+
+            $ul.append($error);
+            $error.hide().show().fadeOut(0).fadeIn('fast');
+
+            $.validator.inputs[$(obj).attr('name')].hasErrors = true;
         }
     }
 
-    function format(obj, text) {
-        return text.replace(/\${(.*)}/, capitalize($(obj).attr('name')));
+    function format(obj, text, replacement) {
+        if(replacement){
+            for(var i in replacement) {
+                var re = new RegExp("\\${(" + i + ")}", 'gi');
+
+                if(i === 'field') {
+                    text = text.replace(re, capitalize(replacement[i]));
+                }
+                else {
+                    text = text.replace(re, replacement[i]);
+                }
+            }
+
+            return text;
+        }
+
+        return text.replace(/\${(field)}/, capitalize($(obj).attr('name')));
     }
 
     function capitalize(text) {
